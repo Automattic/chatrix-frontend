0a1082ff1eb01bf2e2803926deedc39cfe6233e0
Paulo Pinto <paulo.pinto@automattic.com>
2022-09-19 16:05:47 +0100

    Undo changes to ChatterboxView.ts

--- src/ui/views/ChatterboxView.ts
+++ src/ui/views/ChatterboxView.ts
@@ -14,8 +14,10 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import { AvatarView, MessageComposer, TemplateView, TimelineView, viewClassForTile } from "hydrogen-view-sdk";
+import { TemplateView, TimelineView, AvatarView, viewClassForTile } from "hydrogen-view-sdk";
+import { MessageComposer } from "hydrogen-view-sdk";
 import { ChatterboxViewModel } from "../../viewmodels/ChatterboxViewModel";
+import { FooterView } from "./FooterView";
 import { LoadingView } from "./LoadingView";
 
 export class ChatterboxView extends TemplateView<ChatterboxViewModel> {
@@ -23,8 +25,9 @@ export class ChatterboxView extends TemplateView<ChatterboxViewModel> {
         super(value);
     }
 
-    render(t) {
-        return t.div({className: "ChatterboxView"}, [
+    render(t, vm) {
+        return t.div({ className: "ChatterboxView", },
+            [
             t.mapView(
                 (vm) => (vm.roomViewModel ? vm : null),
                 (vm) => (vm ? new RoomHeaderView(vm) : null)
@@ -35,8 +38,9 @@ export class ChatterboxView extends TemplateView<ChatterboxViewModel> {
             ),
             t.mapView(
                 (vm) => vm.messageComposerViewModel,
-                (vm) => (vm?.kind === "composer" ? new MessageComposer(vm) : null)
+                (vm) => (vm?.kind === "composer" ? new MessageComposer(vm) : new WaitingForOperatorJoinView())
             ),
+            t.view(new FooterView(vm.footerViewModel)),
         ]);
     }
 }
@@ -47,14 +51,11 @@ class RoomHeaderView extends TemplateView<ChatterboxViewModel> {
     }
 
     render(t, vm: ChatterboxViewModel) {
-        const avatar = vm.customAvatarURL ? t.img({
-            className: "avatar",
-            src: vm.customAvatarURL
-        }) : t.view(new AvatarView(vm.roomViewModel, 30));
-        return t.div({className: "RoomHeaderView"}, [
+        const avatar = vm.customAvatarURL ? t.img({ className:"avatar", src: vm.customAvatarURL }) : t.view(new AvatarView(vm.roomViewModel, 30));
+        return t.div({ className: "RoomHeaderView" }, [
             avatar,
-            t.div({className: "RoomHeaderView_name"}, vm => vm.roomName),
-            t.div({className: "RoomHeaderView_menu"}, [
+            t.div({ className: "RoomHeaderView_name" }, vm => vm.roomName),
+            t.div({ className: "RoomHeaderView_menu" }, [
                 t.button({
                     className: "RoomHeaderView_menu_settings", onClick: () => {
                         vm.showSettings();
@@ -64,8 +65,19 @@ class RoomHeaderView extends TemplateView<ChatterboxViewModel> {
                     className: "RoomHeaderView_menu_minimize", onClick: () => {
                         vm.minimize();
                     }
-                }),
+                })
             ]),
         ]);
+    }
+}
+
+class WaitingForOperatorJoinView extends TemplateView {
+    render(t) {
+        return t.div({ className: "WaitingForOperatorJoinView" }, [
+            t.div({ className: "FakeComposerContainer" }, [
+                t.span("Waiting for operator to join "),
+                t.div({ className: "loader" })]
+            )]
+        );
     }
 }
